6d62746afa410ff248786a3c01395182ffb295f8
Robert Gasior <robertjohngasior@gmail.com>
2025-04-03 17:34:29 +0200

    aadded WSEIGradeBookLogic

---
+++ GradeBook/GradeBooks/WSEIGradeBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class WseiGradeBook : BaseGradeBook
+    {
+        public WseiGradingBook(string bookTitle, bool weighted) : base(bookTitle, weighted)
+        {
+            this.Type = GradeBookType.Wsei;
+        }
+
+        public override char GetLetterGrade(double avgGrade)
+        {
+            return avgGrade switch
+            {
+                >= 90 => '5',
+                >= 80 => 'B',
+                >= 70 => '4',
+                >= 60 => 'C',
+                >= 50 => '3',
+                _ => '2'
+            };
+        }
+    }
+}
