330cc0560a52673b89cbd94a687f9ffbd16c5060
Robert Gasior <robertjohngasior@gmail.com>
2025-04-03 16:43:12 +0200

    moved some cs proj over to different file and made the 1 - 4  gradebook

---
+++ GradeBook/GradeBooks/OneToFourGradeBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using GradeBook.Enums;
+using GradeBook.UserInterfaces;
+
+namespace GradeBook.GradeBooks
+{
+    public class OneToFourGradeBook : BaseGradeBook
+    {
+        public OneToFourGradeBook(string name, bool isWeighted) : base(name, isWeighted)
+        {
+            this.Type = GradeBookType.OneToFour;
+        }
+
+        public override char GetLetterGrade(double avgGrade)
+        {
+            return avgGrade switch
+            {
+                >= 90 => '4',
+                >= 75 => '3',
+                >= 60 => '2',
+                _ => '1'
+            };
+        }
+    }
+}
