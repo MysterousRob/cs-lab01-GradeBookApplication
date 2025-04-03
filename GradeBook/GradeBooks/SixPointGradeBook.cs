6302422c21124d0c8b07c8e989a554b892271009
Robert Gasior <robertjohngasior@gmail.com>
2025-04-03 17:29:36 +0200

    done the sixpointgradebook system

---
+++ GradeBook/GradeBooks/SixPointGradeBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class SixPointGradeBook : BaseGradeBook
+    {
+        public SixPointGradeBook(string name, bool isWeighted) : base(name, isWeighted)
+        {
+            this.Type = GradeBookType.OneToFour;
+        }
+
+        public override char GetLetterGrade(double avgGrade)
+        {
+            return avgGrade switch
+            {
+                >= 90 => '4',
+                >= 75 => '3',
+                >= 60 => '2',
+                _ => '1'
+            };
+        }
+    }
+}
