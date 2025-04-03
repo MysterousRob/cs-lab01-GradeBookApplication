d66a835d5638550b9b28f2c1d8e41c2cdbeb8703
Robert Gasior <robertjohngasior@gmail.com>
2025-04-03 16:22:57 +0200

    added the ensu functionality

---
+++ GradeBook/GradeBooks/ESNUGradeBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class ESNUGradingBook : BaseGradeBook
+    {
+        public ESNUGradingBook(string gradeBookName, bool weighted) : base(gradeBookName, weighted)
+        {
+            this.Type = GradeBookType.ESNU;
+        }
+
+        public override char GetLetterGrade(double avgGrade)
+        {
+            return avgGrade switch
+            {
+                >= 90 => 'E',
+                >= 70 => 'S',
+                >= 50 => 'N',
+                _ => 'U'
+            };
+        }
+    }
+}
