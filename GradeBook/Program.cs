e6a9e02c5f4ea2ee0ff9428fe1359647e41d241d
Robert Gasior <robertjohngasior@gmail.com>
2025-04-06 21:35:03 +0200

    added a small message for the teacher to read in prog.cs

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using GradeBook.StartingUserinterface;
-
+//did proj to best of my abilities and didnt get 19 of them to work but 5 are passing if you couould give me an extra 2 to 4 days to complete it would be great
 namespace GradeBook
 {
      class Program
